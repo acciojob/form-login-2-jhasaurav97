6b9051356f410579f0f1745bb532d0d914a03f3b
Saurav <jhasaurav593@gmail.com>
2025-06-17 00:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const fName = document.getElementById("fName");
+const lName = document.getElementById("lName");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+	alert(`${fName.value} ${lName.value} ${phone.value} ${email.value}`);
+})
